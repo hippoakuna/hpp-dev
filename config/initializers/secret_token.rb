90eae06832a84b2fa0e53416f92138a2277feeb1
hippoakuna <hippoakuna@gmail.com>
2012-11-05 23:03:04 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HppDev::Application.config.secret_token = 'f18e889e4ade096b511b18d33aefefbe964de67cfcd48519faf8531f1e558ed6c49bfed99f7d22d181dd60ead8de0c36934e93d5cf8f56c1895065a68199aefc'
