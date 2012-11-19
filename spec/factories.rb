82b70b3f04fb76f5bd665b9ce97b381308202752
hippoakuna <hippoakuna@gmail.com>
2012-11-19 16:49:32 -0600

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Kalani Akuna"
-    email    "kalaniakuna@gmail.com"
-    password "bender"
-    password_confirmation "bender"
+    sequence(:name)  { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}   
+    password "foobar"
+    password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,16 @@
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
+
+# below was edited out under FactoryGirl
+#do
+ #       fill_in "Email",    with: user.email
+  #      fill_in "Password", with: user.password
+   #     click_button "Sign in"
+    #  end
